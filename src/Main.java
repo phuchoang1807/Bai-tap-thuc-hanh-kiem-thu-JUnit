1988cb0a9ee2619882e4583408f3073bc65dace6
phuchoang1807 <BIT230323@st.cmc-u.edu.vn>
2026-01-13 18:06:52 +0700

    feat: implement countExcellentStudents()

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StudentAnalyzer studentAnalyzer = new StudentAnalyzer();
+
+        List<Double> scores = Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0);
+
+        int excellentCount = studentAnalyzer.countExcellentStudents(scores);
+
+        System.out.println("Danh sách điểm: " + scores);
+        System.out.println("Số lượng sinh viên loại giỏi (>= 8.0): " + excellentCount);
+
+    }
+}
