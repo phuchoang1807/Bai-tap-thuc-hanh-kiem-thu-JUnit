72482cc40118958d2f23618cc80e509fc8290169
phuchoang1807 <BIT230323@st.cmc-u.edu.vn>
2026-01-13 18:08:44 +0700

    feat: implement calculateValidAverage()

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,14 @@ public class Main {
         StudentAnalyzer studentAnalyzer = new StudentAnalyzer();
 
         List<Double> scores = Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0);
+        double averageScore = studentAnalyzer.calculateValidAverage(scores);
+
 
         int excellentCount = studentAnalyzer.countExcellentStudents(scores);
 
         System.out.println("Danh sách điểm: " + scores);
         System.out.println("Số lượng sinh viên loại giỏi (>= 8.0): " + excellentCount);
+        System.out.println("Điểm trung bình hợp lệ: " + averageScore);
 
     }
 }
--- src/StudentAnalyzer.java
+++ src/StudentAnalyzer.java
@@ -14,5 +14,23 @@ public class StudentAnalyzer {
         return count;
     }
 
+    public double calculateValidAverage(List<Double> scores) {
+        if (scores == null || scores.isEmpty()) return 0;
+
+        double sum = 0;
+        int count = 0;
+
+        for (Double score : scores) {
+            if (score == null) continue;
+            if (score < 0 || score > 10) continue;
+
+            sum += score;
+            count++;
+        }
+
+        if (count == 0) return 0;
+        return sum / count;
+    }
+
 
 }
